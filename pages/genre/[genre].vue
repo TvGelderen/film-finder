9d951a3044144193233b2ab0a63c406787417ed2
Timen van Gelderen <tvgelderen@outlook.com>
2023-11-28 21:35:35 +0100

    initial pagination functionality

--- pages/genre/[genre].vue
+++ pages/genre/[genre].vue
@@ -1,9 +1,20 @@
 <template>
     <div class="page-container">
         <h2 class="header">{{ genre }}</h2>
-        <div class="movie-grid">
-            <div v-for="movie in movies">
+        <div class="movie-grid" ref="gridRef">
+            <div class="movie-grid-item" v-for="movie in movies">
                 <MovieCard :movie="movie" />
+            </div>
+        </div>
+        <div class="pagination-container">
+            <div :class="`page-button ${page == 1 ? 'disabled' : ''}`" @click="previousPage">
+                Previous page
+            </div>
+            <div class="current-page">
+                Page: {{ page }}
+            </div>
+            <div class="page-button" @click="nextPage">
+                Next page
             </div>
         </div>
     </div>
@@ -17,9 +28,13 @@ const route = useRoute();
 
 const page = ref(1);
 const genreId = ref(0);
+const gridRef = ref<HTMLElement | null>(null);
+const movies = ref<Movie[] | null>(null);
 
 const genre = computed(() => route.params.genre);
 const url = computed(() => `/api/movies/genre/${genreId.value}?page=${page.value}`);
+
+let fetchSize: number;
 
 useHead({
   title: `FilmFinder - ${genre.value}`,
@@ -37,13 +52,51 @@ if (genre) {
     }
 }
 
-let movies: Movie[];
-
-const { data } = await useFetch<MovieResponse>(url);
+const { data } = await useFetch<Movie[]>(url);
 
 if (data.value) {
-    movies = data.value.results;
+    movies.value = data.value;
+    fetchSize = data.value.length;
 }
+
+const previousPage = async () => {
+    page.value--;
+    scrollToPageTop();
+    await fetchPageData();
+}
+
+const nextPage = async () => {
+    page.value++;
+    scrollToPageTop();
+    await fetchPageData();
+};
+
+const scrollToPageTop = () => {
+    const page = document.getElementsByTagName("main")[0];
+    page.scrollTop = 0;
+}
+
+const fetchPageData = async () => {
+    if (gridRef.value != null) {
+        const movieCard = document.querySelector(".movie-grid-item");
+
+        if (movieCard == null) return;
+
+        const itemsPerRow = Math.floor(gridRef.value.clientWidth / movieCard?.clientWidth);
+        const numberOfRows = 5;
+
+        const response = await $fetch(`/api/movies/genre/${genreId.value}?page=${page.value}&fetchSize=${fetchSize}&count=${itemsPerRow * numberOfRows}`) as Movie[];
+
+        if (response) {
+            console.log(response)
+            movies.value = response;
+        }
+    }
+}
+
+onMounted(async () => {
+    await fetchPageData();
+});
 </script>
 
 <style scoped>
@@ -59,10 +112,34 @@ if (data.value) {
 }
 
 .movie-grid {
-    overflow: auto;
     display: grid;
-    grid-template-columns: repeat(auto-fill, var(--movie-card-width));
-    gap: 0.5rem 1rem;
+    grid-template-columns: repeat(auto-fill, calc(var(--movie-card-width) + 1rem));
+    gap: 0.5rem 0;
     justify-content: center;
+    overflow: auto;
+}
+
+.movie-grid-item {
+    padding: 0.5rem 1rem;
+}
+
+.pagination-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 2rem;
+}
+
+.page-button {
+    width: 8rem;
+    padding: 1rem 0;
+    background-color: var(--theme-color-secondary);
+    cursor: pointer;
+    text-align: center;
+}
+
+.page-button.disabled {
+    opacity: 0.5;
+    pointer-events: none;
 }
 </style>
--- server/api/movies/genre/[genre].ts
+++ server/api/movies/genre/[genre].ts
@@ -1,8 +1,44 @@
+import { Movie, MovieResponse } from "~/types/movie-db/MovieTypes";
 import { fetchFromMovieDB } from "~/utils/fetchFromMovieDB";
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
     const genre = getRouterParam(event, 'genre');
-    const { page } = getQuery(event);
+    const { page, fetchSize, count } = getQuery(event);
 
-    return fetchFromMovieDB(`/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&with_genres=${genre}`);
+    if (!page) return;
+
+    const pageNumber = Number.parseInt(page.toString());
+
+    if (pageNumber <= 0) return;
+
+    if (!fetchSize || !count) {
+        const response = await fetchFromMovieDB(`/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&with_genres=${genre}`) as MovieResponse;
+        return response.results;
+    }
+
+    const fetchSizeNumber = Number.parseInt(fetchSize.toString());
+    const countNumber = Number.parseInt(count.toString());
+    
+    const startPage = Math.max(Math.ceil((pageNumber - 1) * countNumber / fetchSizeNumber), 1);
+    const startIndex = (pageNumber - 1) * countNumber % fetchSizeNumber;
+    const endPage = startPage + Math.ceil((countNumber - (fetchSizeNumber - startIndex)) / fetchSizeNumber); //Math.floor(pageNumber * countNumber / fetchSizeNumber) + 1;
+    const endIndex = (startIndex + countNumber) % fetchSizeNumber == 0 ? fetchSizeNumber : (startIndex + countNumber) % fetchSizeNumber;
+
+    let response: Movie[] | null = null;
+
+    for (let i = startPage; i <= endPage; i++) {
+        const apiResponse = await fetchFromMovieDB(`/discover/movie?include_adult=false&include_video=false&language=en-US&page=${i}&sort_by=popularity.desc&with_genres=${genre}`) as MovieResponse;
+
+        if (response == null) {
+            response = apiResponse.results.slice(startIndex)
+        } else {
+            if (i == endPage) {
+                response.push(...apiResponse.results.slice(0, endIndex));
+            } else {
+                response.push(...apiResponse.results);
+            }
+        }
+    }
+    
+    return response;
 });
