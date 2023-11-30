1f592b97ce5f23d110b99347a0a59be698379999
Timen van Gelderen <tvgelderen@outlook.com>
2023-11-30 07:53:35 +0100

    moved grid to seperate component

---
+++ components/MovieGrid.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="movie-grid" ref="gridRef">
+        <div class="movie-grid-item" v-for="movie in movies">
+            <MovieCard :movie="movie" />
+        </div>
+    </div>
+    <div class="pagination-container">
+        <div :class="`page-button ${page == 1 ? 'disabled' : ''}`" @click="previousPage">
+            Previous page
+        </div>
+        <div class="current-page">
+            Page: {{ page }}
+        </div>
+        <div class="page-button" @click="nextPage">
+            Next page
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Movie } from '~/types/movie-db/MovieTypes';
+
+const props = defineProps({
+    baseEndpoint: {
+        type: String
+    },
+    numberOfRows: {
+        type: Number,
+        default: 5
+    }
+})
+
+const page = ref(1);
+const gridRef = ref<HTMLElement | null>(null);
+const gridItemsPerRow = ref(0);
+const firstItemIndex = ref(0);
+const movies = ref<Movie[] | null>(null);
+
+const itemsOnScreen = computed(() => gridItemsPerRow.value * props.numberOfRows);
+
+let fetchSize: number;
+
+const { data } = await useFetch<Movie[]>(`${props.baseEndpoint}?page=${page.value}`);
+
+if (data.value) {
+    movies.value = data.value;
+    fetchSize = data.value.length;
+}
+
+const previousPage = async () => {
+    page.value--;
+    firstItemIndex.value -= itemsOnScreen.value;
+    scrollToPageTop();
+    await fetchPageData();
+}
+
+const nextPage = async () => {
+    page.value++;
+    firstItemIndex.value += itemsOnScreen.value;
+    scrollToPageTop();
+    await fetchPageData();
+};
+
+const scrollToPageTop = () => {
+    const page = document.getElementsByTagName("main")[0];
+    page.scrollTop = 0;
+}
+
+const fetchPageData = async () => {
+    if (gridRef.value != null) {
+        const gridItem = document.querySelector(".movie-grid-item");
+
+        if (gridItem == null) return;
+
+        gridItemsPerRow.value = Math.floor(gridRef.value.clientWidth / gridItem.clientWidth);
+
+        const response = await $fetch(`${props.baseEndpoint}?page=${page.value}&fetchSize=${fetchSize}&count=${itemsOnScreen.value}&start=${firstItemIndex.value}`) as Movie[];
+
+        if (response) {
+            movies.value = response;
+            console.log(response.length)
+        }
+    }
+}
+
+onMounted(async () => {
+    await fetchPageData();
+
+    window.addEventListener("resize", async () => {
+        const gridItem = document.querySelector(".movie-grid-item");
+
+        if (gridRef.value == null || gridItem == null) return;
+
+        if (Math.floor(gridRef.value.clientWidth / gridItem.clientWidth) != gridItemsPerRow.value) {
+            await fetchPageData();
+        }
+    })
+});
+
+</script>
+
+<style scoped>
+.movie-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, calc(var(--movie-card-width) + 1rem));
+    gap: 0.5rem 0;
+    justify-content: center;
+    overflow: auto;
+}
+
+.movie-grid-item {
+    width: calc(var(--movie-card-width) + 1rem);
+    display: flex;
+    justify-content: center;
+}
+
+.pagination-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 2rem;
+}
+
+.page-button {
+    width: 8rem;
+    padding: 1rem 0;
+    background-color: var(--theme-color-secondary);
+    cursor: pointer;
+    text-align: center;
+    user-select: none;
+}
+
+.page-button.disabled {
+    opacity: 0.5;
+    pointer-events: none;
+}
+</style>
--- pages/genre/[genre].vue
+++ pages/genre/[genre].vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page-container">
         <h2 class="header">{{ genre }}</h2>
-        <div class="movie-grid" ref="gridRef">
+        <!-- <div class="movie-grid" ref="gridRef">
             <div class="movie-grid-item" v-for="movie in movies">
                 <MovieCard :movie="movie" />
             </div>
@@ -16,30 +16,17 @@
             <div class="page-button" @click="nextPage">
                 Next page
             </div>
-        </div>
+        </div> -->
+        <MovieGrid :base-endpoint="`/api/movies/genre/${genreId}`" />
     </div>
 </template>
 
 <script setup lang="ts">
 import type { GenreResponse } from '~/types/movie-db/GenreTypes';
-import type { Movie } from '~/types/movie-db/MovieTypes';
 
 const route = useRoute();
-
-const numberOfRows = 5;
-
-const page = ref(1);
 const genreId = ref(0);
-const gridRef = ref<HTMLElement | null>(null);
-const gridItemsPerRow = ref(0);
-const firstItemIndex = ref(0);
-const movies = ref<Movie[] | null>(null);
-
 const genre = computed(() => route.params.genre);
-const url = computed(() => `/api/movies/genre/${genreId.value}?page=${page.value}`);
-const itemsOnScreen = computed(() => gridItemsPerRow.value * numberOfRows);
-
-let fetchSize: number;
 
 useHead({
   title: `FilmFinder - ${genre.value}`,
@@ -56,70 +43,11 @@ if (genre) {
         }
     }
 }
-
-const { data } = await useFetch<Movie[]>(url);
-
-if (data.value) {
-    movies.value = data.value;
-    fetchSize = data.value.length;
-}
-
-const previousPage = async () => {
-    page.value--;
-    firstItemIndex.value -= itemsOnScreen.value;
-    scrollToPageTop();
-    await fetchPageData();
-}
-
-const nextPage = async () => {
-    page.value++;
-    firstItemIndex.value += itemsOnScreen.value;
-    scrollToPageTop();
-    await fetchPageData();
-};
-
-const scrollToPageTop = () => {
-    const page = document.getElementsByTagName("main")[0];
-    page.scrollTop = 0;
-}
-
-const fetchPageData = async () => {
-    if (gridRef.value != null) {
-        const gridItem = document.querySelector(".movie-grid-item");
-
-        if (gridItem == null) return;
-
-        gridItemsPerRow.value = Math.floor(gridRef.value.clientWidth / gridItem.clientWidth);
-
-        const response = await $fetch(`/api/movies/genre/${genreId.value}?page=${page.value}&fetchSize=${fetchSize}&count=${itemsOnScreen.value}&start=${firstItemIndex.value}`) as Movie[];
-
-        console.log(`/api/movies/genre/${genreId.value}?page=${page.value}&fetchSize=${fetchSize}&count=${itemsOnScreen.value}&start=${firstItemIndex.value}`)
-
-        if (response) {
-            movies.value = response;
-            console.log(response.length)
-        }
-    }
-}
-
-onMounted(async () => {
-    await fetchPageData();
-
-    window.addEventListener("resize", async () => {
-        const gridItem = document.querySelector(".movie-grid-item");
-
-        if (gridRef.value == null || gridItem == null) return;
-
-        if (Math.floor(gridRef.value.clientWidth / gridItem.clientWidth) != gridItemsPerRow.value) {
-            await fetchPageData();
-        }
-    })
-});
 </script>
 
 <style scoped>
 .page-container {
-    width: min(90%, 1800px);
+    width: min(93%, 1800px);
     margin: 2rem auto;
 }
 
@@ -127,38 +55,5 @@ onMounted(async () => {
     font-size: 2rem;
     margin-bottom: 2rem;
     margin-left: 0.5rem;
-}
-
-.movie-grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, calc(var(--movie-card-width) + 1rem));
-    gap: 0.5rem 0;
-    justify-content: center;
-    overflow: auto;
-}
-
-.movie-grid-item {
-    padding: 0.5rem 1rem;
-}
-
-.pagination-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-top: 2rem;
-}
-
-.page-button {
-    width: 8rem;
-    padding: 1rem 0;
-    background-color: var(--theme-color-secondary);
-    cursor: pointer;
-    text-align: center;
-    user-select: none;
-}
-
-.page-button.disabled {
-    opacity: 0.5;
-    pointer-events: none;
 }
 </style>
